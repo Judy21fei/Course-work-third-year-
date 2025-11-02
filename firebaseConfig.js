4624a23b7b1eaf7a557742c64d1ffcdaaa6711ab
judy_u <yuliana.zimirska@lnu.edu.ua>
2025-11-02 18:39:11 +0200

    Add firebase configuration and initialization for authentication

---
+++ firebaseConfig.js
@@ -0,0 +1,25 @@
+// firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+
+// Конфігурація твого проєкту Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyD0-h75n4T5VfRRtoJGLscg6_XBbhVJlq0",
+  authDomain: "accessiblehubapp.firebaseapp.com",
+  projectId: "accessiblehubapp",
+  storageBucket: "accessiblehubapp.firebasestorage.app",
+  messagingSenderId: "499498946357",
+  appId: "1:499498946357:web:69b6de9b464ee5cff3867c",
+  measurementId: "G-9QDFWE15RH",
+};
+
+// Ініціалізація Firebase
+const app = initializeApp(firebaseConfig);
+
+// ✅ Ініціалізація Firebase Auth із збереженням стану користувача
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
+
+export default app;
