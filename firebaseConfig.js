5368a64d23fa2e1c4845959872d5b5c210ca750e
Judy21fei <yuliana.zimirska@lnu.edu.ua>
2025-12-06 11:47:49 +0200

    Obfuscate Firebase configuration values
    Replaced sensitive Firebase configuration values with placeholders for security.

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,13 +4,13 @@ import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0-h75n4T5VfRRtoJGLscg6_XBbhVJlq0",
-  authDomain: "accessiblehubapp.firebaseapp.com",
-  projectId: "accessiblehubapp",
-  storageBucket: "accessiblehubapp.firebasestorage.app",
-  messagingSenderId: "499498946357",
-  appId: "1:499498946357:web:69b6de9b464ee5cff3867c",
-  measurementId: "G-9QDFWE15RH",
+  apiKey: "***",
+  authDomain: "***",
+  projectId: "***",
+  storageBucket: "***",
+  messagingSenderId: "***7",
+  appId: "***",
+  measurementId: "***",
 };
 
 const app = initializeApp(firebaseConfig);
